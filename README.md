33fcdc947ad007b1812f9c87673ab7854926adbd
youngmikec <michaelozor15@gmail.com>
2022-11-16 11:47:50 +0100

    modified backend endpoint to render

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SeafoodFrontend
+# Turah International Logistic Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.12.
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  CURRENT_URL: 'https://seafood-backend.herokuapp.com/api',
+  // CURRENT_URL: 'https://seafood-backend.herokuapp.com/api',
+  CURRENT_URL: 'https://seafood-backend.onrender.com/api',
   API_KEY: 'FREEXIT-e6db11d17773336208de8cb-Z'
 
 };
